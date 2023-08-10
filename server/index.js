10ba7c98703f08d41add88241829b3d3864adf41
You Song Hou <ysongweb3@gmail.com>
2023-08-10 01:31:44 -0400

    feat: create an API for sending Ether

--- server/index.js
+++ server/index.js
@@ -1,12 +1,49 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const ethers = require('ethers');
 
 const app = express();
+
+const privateKey = process.env.PRIVATE_KEY;
+const nodeURL = process.env.GOERLI_NODE;
+
+const provider = new ethers.providers.JsonRpcProvider(nodeURL);
+const wallet = new ethers.Wallet(privateKey, provider);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get("/sendfund", async (req, res) => {
+  try {
+    // const to = req.body.to;
+    // const amount = req.body.amount;
+
+    const gasPrice = await provider.getGasPrice();
+
+    const tx = {
+      from: wallet.address,
+      to: wallet.address,
+      value: ethers.utils.parseUnits("0.0001", "ether"),
+      gasPrice: gasPrice,
+      gasLimit: ethers.utils.hexlify(100000),
+      nonce: await provider.getTransactionCount(
+        wallet.address,
+        "latest"
+      )
+    }
+  
+    const transaction = await wallet.sendTransaction(tx);
+    const data = await transaction.wait();
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
 
 app.get('/', (req, res) => res.send('It Work'));
 
