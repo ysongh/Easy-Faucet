4d1c94693f394bd64ac1b050e228d179451aef81
You Song Hou <ysongweb3@gmail.com>
2023-08-11 00:07:26 -0400

    refactor: update deploy.js

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const faucetNFT = await hre.ethers.deployContract("FaucetNFT");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await faucetNFT.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `FaucetNFT deployed to ${faucetNFT.target}`
   );
 }
 
