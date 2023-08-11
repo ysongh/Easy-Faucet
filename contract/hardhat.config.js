11f7b3629dc93d44d36dd25f6e375358f3ce4d4d
You Song Hou <ysongweb3@gmail.com>
2023-08-11 01:21:16 -0400

    feat: update 'hardhat.config.js' to deploy a contract on Mode Testnet

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,6 +3,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network mode
+    "mode": {
+      url: "https://sepolia.mode.network/",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 919,
+      gasPrice: 8000000000
+    }
+  },
   // set the path to compile the contracts
   paths: {
     artifacts: '../frontend/src/artifacts',
