da7212e2c2963681a76331227acb54920546b9a0
You Song Hou <ysongweb3@gmail.com>
2023-08-13 01:55:53 -0400

    feat: display the transaction hash

--- frontend/src/pages/MintNFT.jsx
+++ frontend/src/pages/MintNFT.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import { Container, Heading, Text, Button } from '@chakra-ui/react';
 
 function MintNFT({ ethAddress, contractNFT}) {
+  const [transactionHash, setTransactionHash] = useState('');
+
   const mint = async () => {
     try {
       const transaction = await contractNFT.issueNFT(ethAddress);
       const tx = await transaction.wait();
       console.log(tx);
+      setTransactionHash(tx.transactionHash);
     } catch(error) {
       console.error(error);
     }
@@ -19,6 +22,12 @@ function MintNFT({ ethAddress, contractNFT}) {
       <Button onClick={mint} mt='4'>
         Mint
       </Button>
+      {transactionHash &&
+        <Text mt='3' color='green.500'>
+          Success, {' '}
+          {transactionHash.substring(0, 10) + '...' + transactionHash.substring(56, 66)}
+        </Text>
+      }
     </Container>
   )
 }
